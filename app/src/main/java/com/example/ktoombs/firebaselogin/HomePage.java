1731364cb64a64d04580faaf04486e6048facef6
ktoombs <kctoombs@charter.net>
2017-11-29 22:35:11 -0800

    Adding favorites and workout tabs to homepage.

--- app/src/main/java/com/example/ktoombs/firebaselogin/HomePage.java
+++ app/src/main/java/com/example/ktoombs/firebaselogin/HomePage.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -16,6 +17,8 @@ import com.google.firebase.auth.FirebaseAuth;
 public class HomePage extends AppCompatActivity {
 
     private Button signOut;
+    private ImageButton favorites;
+    private ImageButton workouts;
     FirebaseAuth mAuth;
 
     @Override
@@ -27,6 +30,24 @@ public class HomePage extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        workouts = findViewById(R.id.workouts);
+        workouts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent workoutsIntent = new Intent(Workouts.class);
+                //startActivity(workoutsIntent);
+            }
+        });
+
+        favorites = findViewById(R.id.favorites);
+        favorites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent favoritesIntent = new Intent(Favorites.class);
+                //startActivity(favoritesIntent);
+            }
+        });
 
         signOut = findViewById(R.id.signOut);
         signOut.setOnClickListener(new View.OnClickListener() {
