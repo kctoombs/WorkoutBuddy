56db969a0a2c9b24c6f85bf336b2c72a138b75a5
ktoombs <kctoombs@charter.net>
2017-11-30 22:40:07 -0800

    Created workouts activity. Created bottom toolbar layout. Added home button to bottom toolbar.

---
+++ app/src/main/java/WorkoutsActivity.java
@@ -0,0 +1,14 @@
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ktoombs on 11/30/2017.
+ */
+
+public class WorkoutsActivity extends AppCompatActivity {
+    //Chest
+    //Back
+    //Shoulders
+    //Biceps
+    //Triceps
+    //Legs
+}
